4576633d6d04f88b8031c922c24f31f0e671f6ba
Arsenty Dafintsev <aaapython24@gmail.com>
2025-05-31 12:46:41 +0300

    Fix name field for models
    Fix migrations

--- catalog/models.py
+++ catalog/models.py
@@ -2,20 +2,20 @@ from django.db import models
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100, verbose_name='Имя_категории')
+    name = models.CharField(max_length=100, verbose_name='Имя_категории')
     description = models.TextField(verbose_name='Описание_категории')
 
     def __str__(self):
-        return f'{self.category_name}'
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        ordering = ['category_name']
+        ordering = ['name']
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=100, verbose_name='Имя_продукта')
+    name = models.CharField(max_length=100, verbose_name='Имя_продукта')
     description = models.TextField(verbose_name='Описание_продукта')
     image = models.ImageField(upload_to='product_images/', verbose_name='Изображение_продукта', blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
@@ -24,9 +24,9 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.product_name} {self.category}'
+        return f'{self.name} {self.category}'
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
-        ordering = ['product_name', 'category', 'price']
+        ordering = ['name']

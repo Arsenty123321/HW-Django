47d6601356a31c6c799f5ac8411b822fdf5e28c9
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-05 01:40:33 +0300

    Add new controllers and templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from gc import get_objects
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def home(reqiest):
@@ -7,3 +10,15 @@ def home(reqiest):
 
 def contacts(reqiest):
     return render(reqiest, 'contacts.html')
+
+
+def product_list(reqiest):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(reqiest, 'product.html', context)
+
+
+def product_detail(reqiest, pk):
+    product = get_object_or_404(Product, id=pk)
+    context = {'product': product}
+    return render(reqiest, 'product_detail.html', context)

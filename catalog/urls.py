6474fa4c878eef20831c4eeeb098bb742e0bd8ec
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-08 23:38:15 +0300

    Refactoring FBV to CBV for the Catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import home, contacts, product_detail, product_list
+from .views import ProductListView, ProductDetailView, HomeTemplateView, ContactsTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product_list/', product_list, name='product_list'),
-    path('product_detail/<int:pk>/', product_detail, name='product_detail'),
+    path('', HomeTemplateView.as_view(), name='home'),
+    path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
+    path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]

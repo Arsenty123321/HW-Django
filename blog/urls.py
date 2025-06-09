0862d0d78e0db263fdc0e9250c5991d77108a920
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-09 11:01:22 +0300

    Add the BlogDetailView class and templating

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import BlogListView
+from blog.views import BlogListView, BlogDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('blogs_list/', BlogListView.as_view(), name='blogs_list'),
+    path("blog/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Blog
 
@@ -9,3 +9,14 @@ class BlogListView(ListView):
 
     def get_queryset(self):
         return Blog.objects.filter(is_published=True)
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    context_object_name = "blog"
+
+    def get_object(self, queryset=None):
+        blog = super().get_object(queryset)
+        blog.views_count += 1
+        blog.save()
+        return blog

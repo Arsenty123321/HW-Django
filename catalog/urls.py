5b38da74e81ca7fac15a31859e2077fc7ead1917
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-12 23:56:42 +0300

    CRUD refactoring to the catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from .views import ProductListView, ProductDetailView, HomeTemplateView, ContactsTemplateView
+from .views import ProductListView, ProductDetailView, HomeTemplateView, ContactsTemplateView, ProductCreateView, \
+    ProductUpdateView, ProductDeleteView
 
 app_name = CatalogConfig.name
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
     path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path("create/", ProductCreateView.as_view(), name="product_create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product_update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]

056c1f82efc20eab9cd3266cb482aab289170793
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-13 01:35:49 +0300

    Add form styling

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
 from .models import Product
 
 FORBIDDEN_WORDS = [
@@ -14,7 +14,17 @@ FORBIDDEN_WORDS = [
 ]
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"

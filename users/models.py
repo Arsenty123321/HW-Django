9bc32ad2e2306ef5631579cda9f07b2d6ae9cff0
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-14 01:01:40 +0300

    Add the User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    avatar = models.ImageField(upload_to="users_avatars/", verbose_name="Аватар", blank=True,
+                               help_text="Загрузите свой аватар")
+    phone = PhoneNumberField(verbose_name="Телефон", blank=True, help_text="Введите номер телефона")
+    country = models.CharField(max_length=35, verbose_name="Страна", blank=True, help_text="Введите название страны")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email

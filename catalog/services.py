5c4c15f24e0e65a0170d9f9dc402ca7e25e6e6c8
Arsenty Dafintsev <aaapython24@gmail.com>
2025-06-21 17:57:38 +0300

    Add category: func, view, templating

--- catalog/services.py
+++ catalog/services.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 
-from catalog.models import Product
+from catalog.models import Product, Category
 from config.settings import CACHE_ENABLED
 
 
@@ -20,3 +20,27 @@ def get_products_from_cache():
     products = Product.objects.all()
     cache.set(key, products, 60 * 15)
     return products
+
+def get_categories_from_cache():
+    """
+        Получает данные из кэша, если пуст, то кеширует возврат из БД.
+    """
+
+    if not CACHE_ENABLED:
+        return Category.objects.all()
+
+    key = "category_list"
+    categories = cache.get(key)
+
+    if categories is not None:
+        return categories
+    categories = Category.objects.all()
+    cache.set(key, categories, 60 * 15)
+    return categories
+
+def get_products_from_category(category_id):
+    """
+        Возвращает список продуктов из категории.
+    """
+
+    return Product.objects.filter(category_id=category_id)
